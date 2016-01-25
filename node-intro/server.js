ba7e966dcf7cac1df5a7fc47aa6623cc50985c56
sanjaysagarp <sanjaysagarp@hotmail.com>
2016-01-25 08:17:55 -0800

    testing uploads in node

--- node-intro/server.js
+++ node-intro/server.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var multer  =   require('multer');
 
 //create a new express application
 var app = express();
@@ -15,6 +16,18 @@ app.use(bodyParser.json());
 
 //serve static files from /static
 app.use(express.static(__dirname + '/static'));
+
+var storage =   multer.diskStorage({
+	destination: function (req, file, callback) {
+		callback(null, './uploads');
+		},
+		filename: function (req, file, callback) {
+			callback(null, file.fieldname + '-' + Date.now());
+		}
+});
+
+
+//var upload = multer({ storage : storage}).single('userPhoto');
 
 /*
 //call this function for GET on /
@@ -29,6 +42,21 @@ app.get('/time', function(req, res) {
 });
 */
 
+/*app.post('/api/photo',function(req,res){
+	upload(req,res,function(err) {
+		if(err) {
+			return res.end("Error uploading file.");
+		}
+		res.end("File is uploaded");
+	});
+});*/
+
+var upload = multer({ dest: 'uploads/' })
+app.post('/api/photo', upload.single('userPhoto'), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+});
+
 app.get('/api/v1/users', function(req, res) {
 	var users = [
 		{
@@ -42,7 +70,6 @@ app.get('/api/v1/users', function(req, res) {
 
 app.post('/api/v1/users', function(req, res) {
 	console.log(req.body);
-	
 	res.json({message: 'new user created'});
 });
 
